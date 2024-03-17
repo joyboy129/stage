fc3dccd23f1668ff95d56a4553f8932af96a4c12
joyboy129 <104608855+joyboy129@users.noreply.github.com>
2024-03-17 15:22:49 +0000

    Add files via upload

--- Data_extraction.py
+++ Data_extraction.py
@@ -25,6 +25,36 @@ def get_next_code(code):
             return ''.join(code)
         
         
+# def snap_table(sheet, topleft, num_columns, num_rows):
+#     '''
+#     input:
+#         sheet: the Excel sheet we want
+#         topleft: coordinates of topleft coordinate eg. 'B2'
+#         num_columns: number of columns in table
+#         num_rows: number of rows in table
+#     output:
+#         pandas dataframe representing table
+#     '''
+#     try:
+#         import re
+#         col = re.findall('[a-zA-Z]+', topleft)[0]
+#         num = int(re.findall('[0-9]+', topleft)[0])
+
+#         columns = [col]
+#         for i in range(num_columns-1):
+#             columns.append(get_next_code(columns[-1]))
+#         numbers = [n for n in range(num, num+num_rows)]
+
+#         data = []
+#         for n in numbers:
+#             row = []
+#             for c in columns:
+#                 code = c + str(n)
+#                 row.append(sheet[code].value)
+#             data.append(row)
+#         return pd.DataFrame(data[1:], columns=data[0])
+#     except Exception as e:
+#         return pd.DataFrame()
 def snap_table(sheet, topleft, num_columns, num_rows):
     '''
     input:
@@ -50,19 +80,20 @@ def snap_table(sheet, topleft, num_columns, num_rows):
             row = []
             for c in columns:
                 code = c + str(n)
-                row.append(sheet[code].value)
+                row.append(sheet[code].value or sheet[code].value)
             data.append(row)
         return pd.DataFrame(data[1:], columns=data[0])
     except Exception as e:
         return pd.DataFrame()
+
     
 # load path to excel file + select your excel sheet
-workbook = load_workbook('beginning.xlsx') 
+workbook = load_workbook('Modèle_Belfaa_updated_linearized_1303_simul_noQ.xlsx') 
 sheet1 = workbook['latest inputs']
 sheet2=workbook['Indices']
 sheet3=workbook['Simulation']
 df_price=snap_table(sheet1, 'B10',91, 3)
-df_prod=snap_table(sheet1, 'B17',45, 15)
+df_prod=snap_table(sheet1, 'B17',45, 16)
 df_chargesvar=snap_table(sheet1, 'B38',8, 16)
 df_plantation=snap_table(sheet2, 'J52',3, 11)
 df_month_index=snap_table(sheet2, 'L3',3, 25)
